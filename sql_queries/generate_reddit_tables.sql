6377afb22847243347a7e21ed248ac6bc741d16b
Albert Gertskis <albertgertskis@gmail.com>
2018-12-04 19:20:13 -0500

    Added database queries
    This includes a query to create the 'reddit' DB and it's respective tables as well as a query to drop the 'reddit' db for easier development

---
+++ sql_queries/generate_reddit_tables.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE reddit;
+USE reddit;
+
+-- Stores information of a 'Reddit user'
+CREATE TABLE users(
+    user_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
+    user_username VARCHAR(25) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    cake_day DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    total_karma INT NOT NULL, -- Aggregate amount of karma user earned from posts and comments
+    PRIMARY KEY (user_id)
+);
+
+/*
+ * Stores information of a "Reddit post' made by a 'Reddit user'
+ * NOTE, if 'Reddit user' is deleted from the DB, the 'Reddit posts' made
+ * by that user are deleted
+*/
+CREATE TABLE posts(
+    post_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
+    post_title VARCHAR(300) NOT NULL,
+    post_image VARCHAR(1000), -- This is a link to the actual image, supplied by the user (used for posts only to display the actual image within the post, rather than having the user click on the link to see the image)
+    post_content VARCHAR(40000),
+    post_karma INT UNSIGNED DEFAULT 1, -- Posts start with a default karma value of 1 (upvote button automatically clicked; if clicked again, karma value decrements by 1 to 0)
+    post_timeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+/*
+ * Stores information of a "Reddit comment' made by a 'Reddit user'on a 'Reddit post'.
+ * NOTE, if 'Reddit user' is deleted from the DB, the 'Reddit comments' made
+ * by that user are deleted
+*/
+CREATE TABLE comments (
+    comment_id INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
+    comment_content VARCHAR(10000) NOT NULL,
+    comment_karma INT UNSIGNED DEFAULT 1, -- Comments start with a default karma value of 1 (upvote button automatically clicked; if clicked again, karma value decrements by 1 to 0)
+    comment_timeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    post_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, -- If a user is deleted, deleted all their posts
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE -- If a post is deleted, deleted the post and all of it's content
+);
+
+
